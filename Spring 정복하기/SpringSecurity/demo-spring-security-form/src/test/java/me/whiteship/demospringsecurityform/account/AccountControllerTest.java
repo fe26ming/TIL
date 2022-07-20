a806e8f8d8d1d7f2955b9ee4546f66563ae0c828
Fe26Atom <pcm0720@gmail.com>
2022-07-20 10:58:04 +0900

    TIL - 스프링 시큐리티 테스트 2부
    TIL - 스프링 시큐리티 테스트 2부

--- Spring 정복하기/SpringSecurity/demo-spring-security-form/src/test/java/me/whiteship/demospringsecurityform/account/AccountControllerTest.java
+++ Spring 정복하기/SpringSecurity/demo-spring-security-form/src/test/java/me/whiteship/demospringsecurityform/account/AccountControllerTest.java
@@ -9,8 +9,13 @@ import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -23,6 +28,9 @@ public class AccountControllerTest {
 
     @Autowired
     MockMvc mockMvc;
+
+    @Autowired
+    AccountService accountService;
 
     @Test
     @WithAnonymousUser
@@ -39,7 +47,7 @@ public class AccountControllerTest {
     public void index_user() throws Exception {
         mockMvc.perform(get("/"))
                 .andDo(print())
-                .andExpect(status().isForbidden());
+                .andExpect(status().isOk());
     }
 
     @Test
@@ -58,4 +66,29 @@ public class AccountControllerTest {
                 .andExpect(status().isOk());
     }
 
+
+    @Test
+    @Transactional
+    public void login_success() throws Exception{
+        Account user = this.createUser();
+        mockMvc.perform(formLogin().user(user.getUsername()).password("123"))
+                .andExpect(authenticated());
+    }
+
+    @Test
+    @Transactional
+    public void login_fail() throws Exception{
+        Account user = this.createUser();
+        mockMvc.perform(formLogin().user(user.getUsername()).password("12345"))
+                .andExpect(unauthenticated());
+    }
+
+    private Account createUser() {
+        Account account = new Account();
+        account.setUsername("fe26ming");
+        account.setPassword("123");
+        account.setRole("USER");
+        accountService.createNew(account);
+        return account;
+    }
 }
