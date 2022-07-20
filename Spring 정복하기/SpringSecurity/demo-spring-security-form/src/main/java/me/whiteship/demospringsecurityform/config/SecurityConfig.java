6f891204bc121940ebd98ba9475c587a3b60ab58
Fe26Atom <pcm0720@gmail.com>
2022-07-20 15:23:31 +0900

    TIL - AccessDecisionManager 2부
    TIL - AccessDecisionManager 2부

--- Spring 정복하기/SpringSecurity/demo-spring-security-form/src/main/java/me/whiteship/demospringsecurityform/config/SecurityConfig.java
+++ Spring 정복하기/SpringSecurity/demo-spring-security-form/src/main/java/me/whiteship/demospringsecurityform/config/SecurityConfig.java
@@ -5,24 +5,44 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.access.AccessDecisionManager;
+import org.springframework.security.access.AccessDecisionVoter;
+import org.springframework.security.access.expression.SecurityExpressionHandler;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.access.vote.AffirmativeBased;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
+import org.springframework.security.web.access.expression.WebExpressionVoter;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @Order(Ordered.LOWEST_PRECEDENCE - 50)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    public SecurityExpressionHandler expressionHandler(){
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
+
+        DefaultWebSecurityExpressionHandler handler = new DefaultWebSecurityExpressionHandler();
+        handler.setRoleHierarchy(roleHierarchy);
+
+       return handler;
+    }
+
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http
-                .antMatcher("/**")
-                    .authorizeRequests()
-                    .mvcMatchers("/", "/info", "/account/**").permitAll()
-                    .mvcMatchers("/admin").hasRole("ADMIN")
-                    .anyRequest().authenticated();
+        http.authorizeRequests()
+                .mvcMatchers("/", "/info", "/account/**").permitAll()
+                .mvcMatchers("/admin").hasRole("ADMIN")
+                .mvcMatchers("/user").hasRole("USER")
+                .anyRequest().authenticated()
+                .expressionHandler(expressionHandler());
 
         http.formLogin();
         http.httpBasic();
