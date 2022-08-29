1fde7b5bc6bd2094b5b558f0705158725a970033
Fe26Atom <pcm0720@gmail.com>
2022-08-29 17:10:48 +0900

    스프링 입문 7 - AOP
    스프링 입문 7 - AOP

--- Spring 정복하기/영한님의 스프링 완전 정복/스프링 입문/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ Spring 정복하기/영한님의 스프링 완전 정복/스프링 입문/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,5 +1,6 @@
 package hello.hellospring;
 
+import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
@@ -45,6 +46,11 @@ public class SpringConfig {
         return new MemberService(memberRepository);
     }
 
+
+//    @Bean
+//    public TimeTraceAop timeTraceAop() {
+//        return new TimeTraceAop();
+//    }
 
 //    @Bean
 //    public MemberRepository memberRepository() {
